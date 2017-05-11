5db12614d42655f851f8ccae00267184a0fb7b53
DonSalvadori <csalvi42@gmail.com>
2017-05-11 08:42:04 -0700

    missed a letter in config

--- src/core/firebase/config.js
+++ src/core/firebase/config.js
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   apiKey: 'AIzaSyDznC6SgMJbq8s9ipNE_1OhB28_p00kVkk',
   authDomain: 'todom-b01a6.firebaseapp.com',
-  databaseURL: 'https://todom-b01a6.firebaseio.co',
+  databaseURL: 'https://todom-b01a6.firebaseio.com',
   storageBucket: 'todom-b01a6.appspot.com'
 };
